22194989e193adb32e8d00183efdb29781b8ffc5
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-04-17 17:00:48 -0500

    machine learning

--- ml.playground/src/main/java/dev/research/himanshu/ml/playground/textclassification/utils/Constants.java
+++ ml.playground/src/main/java/dev/research/himanshu/ml/playground/textclassification/utils/Constants.java
@@ -13,5 +13,5 @@ public class Constants {
 	public static String NUMERICS = "[- ./ 0-9]+";
 	public static String TRAIN = "train";
 	public static String TEST = "test";
-	
+	public static String ARFF_EXTENSION = ".arff";
 }
